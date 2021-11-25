539da0374f35be6661834c7d1a010180a1f46b26
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2021-11-25 12:09:22 +0300

    added all tasks

--- ex_06.py
+++ ex_06.py
@@ -2,3 +2,13 @@ name = input('Enter your name:\n')
 age = int(input('Enter your age:\n'))
 print(f'{name} is {age} year old')
 print('{} is {:b} years old'.format(name.capitalize(), age))
+
+paragraph = '\n"Python is a great language!", said Fred. "I don`t'' \never remember having this much fun before."'
+print(paragraph)
+
+print("\n\u03F4")
+print("\N{GREEK CAPITAL THETA SYMBOL}")
+
+item = 'car'
+car = 13499.99
+print(f"\n{item:<10}{car:>10,.2f}")
