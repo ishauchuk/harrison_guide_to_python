0eae3079da1db62409e7af90257c571f4de5ac25
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2021-11-25 16:07:59 +0300

    added some exercises

---
+++ ex_07.py
@@ -0,0 +1,11 @@
+school = 'Gymnasium №2'
+print(help(dir(school)))
+
+country = 'usa'
+correct_country = country.upper()
+print(correct_country)
+
+filename = 'hello.py'
+print(filename.endswith('java'))
+print(filename.find('py'))
+print(filename.startswith('world'))
