7491e8df2e1ed911cbf3f0fe2b66e052af05fb04
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2021-11-26 15:59:16 +0300

    added new tasks

---
+++ ex_08.py
@@ -0,0 +1,20 @@
+import string
+
+age = 29
+old = age > 18
+print(True if old else False)
+
+name = 'Ihar'
+second_half = list(string.ascii_lowercase)[13:]
+print(True if name[0].lower() in second_half else False)
+
+
+names = [['Alex', 'Daria', 'Serg'], []]
+num = 1
+
+for i in names:
+    if i:
+        print(f"The class {num} has enrollments.")
+    else:
+        print(f"The class {num} is empty!")
+    num += 1
