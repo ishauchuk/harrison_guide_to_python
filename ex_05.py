31ac06417d9e31e0e1f67135aee4614fe2b43589
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2021-11-28 16:38:31 +0300

    bug fixed

--- ex_05.py
+++ ex_05.py
@@ -6,5 +6,5 @@ print(297 % 3 == 0)
 print(2 ** 10)
 
 for elem in [1800, 1900, 1903, 2000, 2002]:
-    if elem % 4 == 0 and elem % 400 == 0:
+    if elem % 4 == 0 and elem % 100 != 0 or elem % 400 == 0:
         print(f'{elem} is bis sextus year')
